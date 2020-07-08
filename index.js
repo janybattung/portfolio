6d2e25b1a82b5eea7eb47cc85fbce2e4660dbdac
Jany Battung <janybattung@gmail.com>
2020-07-08 02:30:47 -0700

    update files

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
     console.log("document ready");
     $("#about").click( function(event) {
-        alert("click");
+    $('html,body').animate({
+        scrollTop:$('.aboutUs').offset().top
+    },500);
+    });
+    $('#projects').click(function(event) {
+    $('html,body').animate({
+        scrollTop:$('.project').offset().top
+        },500);    
+    });
+
+    $('#contact').click( function(event) {
+        $('html,body').animate({
+            scrollTop:$('.contact').offset().top
+            },500);    
+        });
+    $('.hamburger').click(function(event) {
+        console.log('i am working');
+        event.preventDefault();
+        
+    
     });
 });
