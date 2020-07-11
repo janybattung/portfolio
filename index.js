68d25470f45b5493aa72cf6712397fdfb4322cf5
Jany Battung <janybattung@gmail.com>
2020-07-11 01:06:55 -0700

    update file

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
 $(document).ready(function() {
-    console.log("document ready");
-    $("#about").click( function(event) {
-    $('html,body').animate({
+$("#about").click( function(event) {
+$('html,body').animate({
         scrollTop:$('.aboutUs').offset().top
     },500);
-    });
-    $('#projects').click(function(event) {
+});
+$('#projects').click(function(event) {
     $('html,body').animate({
         scrollTop:$('.project').offset().top
-        },500);    
-    });
+    },500);    
+});
 
-    $('#contact').click( function(event) {
-        $('html,body').animate({
+$('#contact').click( function(event) {
+    $('html,body').animate({
             scrollTop:$('.contact').offset().top
             },500);    
-        });
-    $('.hamburger').click(function(event) {
-        console.log('i am working');
-        event.preventDefault();
-        
-    
+});
+$('.hamburger').click(function(event) {
+    $(this).hide();
+    $("#horizontal-list").addClass('active')
+    $("#horizontal-list").animate({
+        right: "0"
+        },600,function() {
+    $(".close").show();
     });
+
+});
+
+$('.close').click(function(event) {
+    $(this).hide();
+$("#horizontal-list").removeClass('active')
+$("#horizontal-list").animate({
+       right: "-80%"
+  },600,function() {
+    $(".hamburger").show();
+    });
+   
+});
+
+$('body').on('click', '#horizontal-list.active li', function() {
+    $("#horizontal-list").removeClass('active')
+    $(".close").hide();
+    $("#horizontal-list").animate({
+        right: "-80%"
+        },600,function() {
+    $(".hamburger").show();
+        });
+   
+    });
+
 });
